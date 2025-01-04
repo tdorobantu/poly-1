d7abe4c5e3f0161e89376fd4ea431a47d269e4c5
tdorobantu <tdorobantu95@gmail.com>
2025-01-04 08:31:38 +0200

    the bot works. orders placed and cancelled

--- index.ts
+++ index.ts
@@ -56,32 +56,43 @@ console.log(collateral);
 // });
 
 // console.log("allowance", no);
-const market = await clobClient.getMarket("0xb7faecd2db357e4634fe87ef9bb48b4e1305cd618276d730b73190662fe6bafd");
-console.log(`market: `);
-console.log(market);
+// const market = await clobClient.getMarket("0xb7faecd2db357e4634fe87ef9bb48b4e1305cd618276d730b73190662fe6bafd");
+// console.log(`market: `);
+// console.log(market);
 
 // Create a buy order for 100 NO for 0.50c
     const NO = '70153859989212405649302660029170495449818146693944403467922191183238786687468';
-    const oneMinute = parseInt(((new Date().getTime() + 60 * 1000 + 10 * 1000) / 1000).toString());
+    // const oneMinute = parseInt(((new Date().getTime() + 60 * 1000 + 10 * 1000) / 1000).toString());
 
     const order = await clobClient.createOrder({
         tokenID: NO,
-        price: 0.20,
-        side: Side.BUY,
+        price: 0.23,
+        side: Side.SELL,
         size: 5,
     });
-    console.log("Created Order", order);
+    // console.log("Created Order", order);
 
     // Send it to the server
-    // const responseOne = await clobClient.postOrder(order, OrderType.GTC);
-    // console.log(responseOne);
+    const responseOne = await clobClient.postOrder(order, OrderType.GTC);
+    console.log(responseOne);
     
+
+
+
+    // const cancelOrder = await clobClient.cancelOrder({
+    //   orderID:
+    //     "0x743c30420927c5c6f230b17d7e4b81a3acdec3d260dbfe0f98f68fd23266d398",
+    // });
 
     const resp = await clobClient.getOpenOrders({
       market:
         "0xb7faecd2db357e4634fe87ef9bb48b4e1305cd618276d730b73190662fe6bafd",
     });
+
     console.log(resp);
     console.log(`Done!`);
+
+    // console.log(cancelOrder);
+    // console.log(`Done!`);
  
     
